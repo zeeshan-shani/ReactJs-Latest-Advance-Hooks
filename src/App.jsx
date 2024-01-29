2f3394dabd9daa253c75437b12f8938719e59f10
Zeeshan <155964871+zeeshanOT@users.noreply.github.com>
2024-01-29 14:43:49 +0500

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Hook from "./components/Hook1";
+function App() {
+  return (
+    <>
+   <Hook/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hook1.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-empty */
+// UseMemo Hook
+import { useState, useMemo } from "react";
+
+const Hook1 = () => {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  //   const doubleNumber = slowFunction(number);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+  const themeStyle = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+  return (
+    <>
+      <h2>useMemo()</h2>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyle}>{doubleNumber}</div>
+    </>
+  );
+};
+
+function slowFunction(num) {
+  console.log("This is slow function");
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2;
+}
+
+export default Hook1;
